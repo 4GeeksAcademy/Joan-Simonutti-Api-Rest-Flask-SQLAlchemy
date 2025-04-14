fbdb67b180a20a413e32a9e91f7fcc03b6b46dde
Joan Simonutti <joansimonutticode@gmail.com>
2025-04-14 09:02:58 +0000

    2

--- newApp.py
+++ newApp.py
@@ -5,9 +5,19 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # endpoints
+
+
 @app.route("/")
 def hello():
-    return jsonify({"msg": "Gex esta aqui!!!"})
+    return jsonify({"msg": "Gex esta aqui!!!"}), 200
+
+@app.route("/numero1", methods=['GET'])
+def handle_numero1():
+    return jsonify({
+        "msg": "numero1 esta aqui, en su endpoint!!!", 
+        "success": True
+        }), 200
+
 
 # ejecucion
 app.run(host='0.0.0.0')
